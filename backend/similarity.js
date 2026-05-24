f4f819ac3a891427ccf28a75bcf691c3782479c3
sandeeprepala <sandeeprepala3@gmail.com>
2026-05-24 17:21:26 +0530

    Checking deletion of stale chunks

--- backend/similarity.js
+++ backend/similarity.js
@@ -1,4 +1,4 @@
-//comment8123
+//comment81234
 export function cosineSimilarity(vecA, vecB) {
     let dot = 0;
     let normA = 0;
--- backend/vector.js
+++ backend/vector.js
@@ -1,4 +1,4 @@
 export const vectordb =()=>{
     console.log("This is a function for vectordb")
 }
-//comment8123
+//comment81234
